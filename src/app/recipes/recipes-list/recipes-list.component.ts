a246e11588c0f18c880497d64ee9682b62d5bd20
HadeelOLotfi <53823794+HadeelOLotfi@users.noreply.github.com>
2023-04-24 20:50:19 +0300

    Add recipe

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
-
+   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply test description', 'https://www.bbcgoodfoodme.com/assets/legacy/recipe/recipe-image/2019/03/hassleback_roast_lamb.jpg')
+   ];
 }
