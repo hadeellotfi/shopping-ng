6cba995d9d80e9181a3f21fc3397253d886942ac
Hadeel Lotfi <34520267+hadeellotfi@users.noreply.github.com>
2023-05-11 14:11:09 +0300

    Passing Ingredients from Recipes to Shopping List

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
-
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -10,24 +11,26 @@ export class RecipeService {
       'A Test Recipe',
       'This is simply test description',
       'https://www.bbcgoodfoodme.com/assets/legacy/recipe/recipe-image/2019/03/hassleback_roast_lamb.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French fries', 20)
-      ]
+      [new Ingredient('Meat', 1), new Ingredient('French fries', 20)]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply test description',
       'https://www.bbcgoodfoodme.com/assets/legacy/recipe/recipe-image/2019/03/hassleback_roast_lamb.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+
+  }
 
   getRecipes() {
     // We got a copy here not a direct ref on our private recipes list
     return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,12 +1,18 @@
+import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-detail',
   templateUrl: './recipes-detail.component.html',
-  styleUrls: ['./recipes-detail.component.css']
+  styleUrls: ['./recipes-detail.component.css'],
 })
 export class RecipesDetailComponent {
   @Input() recipe: Recipe;
 
+  constructor(private recipeService: RecipeService) {}
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientToShoppingList(this.recipe.ingredients);
+  }
 }
